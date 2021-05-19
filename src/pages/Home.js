a4c9ef37bae79a3702be6cde3ff1fa0f2968d2ed
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2021-05-19 15:08:10 +1000

    latest

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styling/Home.css";
 
-function home() {
-  return <div></div>;
+// Redux
+import { connect } from "react-redux";
+
+// Components
+import StepOne from "../components/steps/StepOne";
+import StepTwo from "../components/steps/StepTwo";
+import StepThree from "../components/steps/StepThree";
+
+function Home() {
+  const [currentSection, setCurrentSection] = useState(1);
+
+  return (
+    <div className="Home-page">
+      {currentSection === 1 && <StepOne />}
+
+      {currentSection === 2 && <StepTwo />}
+
+      {currentSection === 3 && <StepThree />}
+    </div>
+  );
 }
 
-export default home;
+export default Home;
